382094e70d4bef7f90b4d1cb108a4f1f0c1cb32d
Adam S. Eidson <104181029+XxMeteoraxX7@users.noreply.github.com>
2022-06-21 19:47:13 -0500

    Update player.js

--- player.js
+++ player.js
@@ -5,6 +5,8 @@ export class Player {
         this.height = 91.3;
         this.x = 0;
         this.y = this.game.height - this.height;
+        this.vy = 0;
+        this.weight = 1;
         this.image = document.getElementById('player');
         this.speed = 0;
         this.maxSpeed = 10;
@@ -17,8 +19,16 @@ export class Player {
        else this.speed = 0;
        if (this.x < 0) this.x = 0;
        if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+       //vertical movement
+        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 10;
+        this.y += this.vy;
+        if (!this.onGround()) this.vy += this.weight;
+        else this.vy = 0;
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    onGround(){
+        return this.y >= this.game.height - this.height;
     }
 }
