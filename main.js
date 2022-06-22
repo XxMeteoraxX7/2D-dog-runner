93df32560caa67ebe3addee7a8478430027bf07e
Adam S. Eidson <104181029+XxMeteoraxX7@users.noreply.github.com>
2022-06-22 17:00:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { Background } from './background.js';
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 500;
+    canvas.width = 800;
     canvas.height = 500;
 
     class Game {
