4be9ae12c3b52b1262b54ed63bbe265c06c6cf8b
Adam S. Eidson <104181029+XxMeteoraxX7@users.noreply.github.com>
2022-06-21 19:36:22 -0500

    Add files via upload

---
+++ player.js
@@ -0,0 +1,24 @@
+export class Player {
+    constructor(game){
+        this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
+        this.speed = 0;
+        this.maxSpeed = 10;
+    }
+    update(input){
+        //horizontal movement
+        this.x += this.speed;
+       if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+       else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+       else this.speed = 0;
+       if (this.x < 0) this.x = 0;
+       if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+    }
+    draw(context){
+        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+}
