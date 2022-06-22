d1036f983387264a5000f0cf5c9aaf2f93d4d864
Adam S. Eidson <104181029+XxMeteoraxX7@users.noreply.github.com>
2022-06-21 21:53:14 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+import { Sitting, Running, Jumping, Falling } from './playerStates.js';
+
 export class Player {
     constructor(game){
         this.game = game;
@@ -8,27 +10,49 @@ export class Player {
         this.vy = 0;
         this.weight = 1;
         this.image = document.getElementById('player');
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame;
+        this.fps = 20;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
-    update(input){
+    update(input, deltaTime){
+        this.currentState.handleInput(input);
         //horizontal movement
         this.x += this.speed;
-       if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
-       else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
-       else this.speed = 0;
-       if (this.x < 0) this.x = 0;
-       if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
-       //vertical movement
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30;
+        if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if (this.x < 0) this.x = 0;
+        if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        //vertical movement
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+        //sprite
+        // if (this.frameTimer > this.frameInterval){
+        //     this.frameTimer = 0;  
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        // } else {
+        //     this.frameTimer += deltaTime;
+        // }
+
     }
     draw(context){
-        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
